08f7a3a63a23cf0f354cb618ccb9823289a8af47
DudaSantoni <159277791+DudaSantoni@users.noreply.github.com>
2025-03-20 16:33:08 -0300

    TechExpo 1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// Arquivo de script vazio 
+let slideIndex = 0;
+const slides = document.querySelector('.gallery-slide');
+const totalSlides = slides.children.length;
+const slideWidth = slides.children[0].offsetWidth + 20; // Considerando margens
+
+document.getElementById('next').addEventListener('click', function() {
+    if (slideIndex < totalSlides - 1) {
+        slideIndex++;
+        slides.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+    }
+});
+
+document.getElementById('prev').addEventListener('click', function() {
+    if (slideIndex > 0) {
+        slideIndex--;
+        slides.style.transform = `translateX(-${slideWidth * slideIndex}px)`;
+    }
+});
